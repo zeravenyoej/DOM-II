ee8b3f97bb3eeef3d6c5ed86b4176ccb95dea4c6
Joey Nevarez <josephnevarez12@gmail.com>
2019-09-03 14:33:05 -0700

    four out of 10 events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 // Your code goes here
+//1. mouseover
+const logo = document.querySelector('.logo-heading');
+logo.addEventListener('mouseover', (event) => {event.target.style.color = "orange"});
+
+//2. mouseenter 
+const headerPic = document.querySelector("header img");
+headerPic.addEventListener('mouseenter', item => {
+    headerPic.style.transform = 'scale(2)';
+})
+
+//3. mouseleave
+const headerPicDown = document.querySelector('header img');
+headerPicDown.addEventListener('mouseleave', item =>{
+    headerPicDown.style.transform = 'scale(1)';
+})
+
+
+//4. load
+window.addEventListener('load', (event) => {
+    console.log('page is fully loaded');
+});
+
+
+
+
+
