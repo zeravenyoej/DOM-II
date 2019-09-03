fefb380fd4e12192b5bc70c9d01192050de23a91
Joey Nevarez <josephnevarez12@gmail.com>
2019-09-03 15:59:01 -0700

    6 events

--- js/index.js
+++ js/index.js
@@ -1,23 +1,24 @@
 // Your code goes here
 //1. mouseover
 const logo = document.querySelector('.logo-heading');
-logo.addEventListener('mouseover', (event) => {event.target.style.color = "orange"});
+logo.addEventListener('mouseover', event => {
+    event.target.style.color = "orange"
+});
 
 //2. mouseenter 
 const headerPic = document.querySelector("header img");
-headerPic.addEventListener('mouseenter', item => {
-    headerPic.style.transform = 'scale(2)';
+headerPic.addEventListener('mouseenter', event => {
+    event.target.style.transform = 'scale(1.1)';
 })
 
 //3. mouseleave
-const headerPicDown = document.querySelector('header img');
-headerPicDown.addEventListener('mouseleave', item =>{
-    headerPicDown.style.transform = 'scale(1)';
+headerPic.addEventListener('mouseleave', event =>{
+    event.target.style.transform = 'scale(1)';
 })
 
 
 //4. load
-window.addEventListener('load', (event) => {
+window.addEventListener('load', event => {
     console.log('page is fully loaded');
 });
 
@@ -27,8 +28,32 @@ btn.forEach(item => {
     item.addEventListener('dblclick', event => {
         event.target.style.color = 'black';
     })
-})
+});
 
+//6. click
+const midPhotos = document.querySelectorAll('.content-section img');
+midPhotos.forEach(photo => {
+    photo.addEventListener('click', event => {
+        event.target.style.transform = 'rotate(90deg)'
+    });
+});
+
+
+//7. contextmenu
+midPhotos.forEach(photo => {
+    photo.addEventListener('auxclick', event => {
+        event.target.style.transform = none;
+    });
+});
+
+
+//8.  contextmenu aka right click
+// const midText = document.querySelectorAll('.content-section p');
+// midText.forEach(text => {
+//     text.addEventListener('contextmenu', event => {
+//         event.target.style.
+//     })
+// })
 
 
 
