3f4582b634b95a025277e01d42325bee8183dba6
Joey Nevarez <josephnevarez12@gmail.com>
2019-10-04 22:07:51 -0700

    added 10 events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,74 @@
 // Your code goes here
+//1. click
+const nav = document.querySelector('.nav-container');
+nav.addEventListener('click', function(){
+    this.style.backgroundColor='red';
+});
+
+//2. mouseover
+const welcome=document.querySelector('.intro h2');
+welcome.addEventListener('mouseover', function(){
+    this.style.display='none';
+})
+
+//3.  mouseleave
+const newLink = document.createElement('a');
+newLink.textContent='Click Me';
+const linkCont = document.querySelector('.nav');
+linkCont.prepend(newLink);
+newLink.classList.add('nav-link');
+newLink.setAttribute('href', '#');
+newLink.addEventListener('mouseleave', function(){
+    this.style.border='3px dashed black';
+});
+
+//4. contextmenu (right click)
+const textPar = document.querySelectorAll('.text-content p');
+for(let i =0;i<textPar.length; i++){
+    textPar[i].addEventListener('contextmenu', function(){
+        this.style.textDecoration='underline';
+    })
+};
+
+//5. keydown
+const body = document.querySelector('body');
+body.addEventListener('keydown', function(){
+    this.style.backgroundColor='beige';
+});
+
+//6. dblclick
+const img = document.querySelectorAll('img');
+for (let i=0; i<img.length; i++){
+    img[i].addEventListener('dblclick', function(){
+        this.style.transform='rotate(90deg)';
+    });
+};
+
+//7. copy
+const footer = document.querySelector('.footer p');
+footer.addEventListener('copy', function(){
+    this.style.letterSpacing = "15px";
+});
+
+//8. mousemove
+const parag = document.querySelectorAll('p');
+for(let i = 0; i<parag.length; i++){
+    parag[i].addEventListener('mousemove', function(){
+        this.style.fontStyle = "italic";
+    })
+};
+
+//9. mousedown
+for(let i = 0; i<parag.length; i++){
+    parag[i].addEventListener('mousedown', function(){
+        this.style.fontStyle = "normal";
+    })
+};
+
+//10. mouseup 
+const destination = document.querySelectorAll('.destination');
+for(let i =0; i<destination.length; i++){
+    destination[i].addEventListener('mouseup', function(){
+        this.style.backgroundColor='yellow';
+    });
+};
